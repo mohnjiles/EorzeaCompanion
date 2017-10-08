8f2c7a802565f6efc63be4fdc11c2c5e3c6c774c
JT Miles <jt@JTs-MacBook-Pro.local>
2017-10-08 13:48:03 -0400

    Initial Commit

---
+++ Eorzea Companion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eorzea Companion
+//
+//  Created by JT Miles on 10/8/17.
+//  Copyright © 2017 JT Miles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
